6a4bd4b6ee5a1db09170c9f44063732b93b72f8e
NadiyaRahmawati <rahmawatinadiya20@mhs.mdp.ac.id>
2022-10-12 17:02:11 +0700

    lanjutan

--- app/src/main/java/com/Nadiya/implicitsi5a/MainActivity.java
+++ app/src/main/java/com/Nadiya/implicitsi5a/MainActivity.java
@@ -1,14 +1,60 @@
 package com.Nadiya.implicitsi5a;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUrl, etLokasi, etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUrl = findViewById(R.id.et_url);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_teks);
+        btnBukaWebsite = findViewById(R.id.btn_buka_website);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlWebsite = etUrl.getText().toString();
+                Uri uriUrlWebsite = Uri.parse(urlWebsite);
+                Intent intenWebsite = new Intent(Intent.ACTION_VIEW, uriUrlWebsite);
+                try {
+                    startActivity(intenWebsite);
+                }catch (Exception e){
+                    etUrl.setError("Url Tidak Sesuai!");
+                }
+
+
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
